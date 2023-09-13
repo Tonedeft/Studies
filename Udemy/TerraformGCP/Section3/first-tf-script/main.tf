6a0ff646e3aacf67baf95e30252dd95abea778a6
Lee Ballard <lee.ballard@yahoo.com>
2023-09-13 13:55:13 -0400

    Section 3-2: Changing resource to local_sensitive_file to protect "content"

--- Udemy/TerraformGCP/Section3/first-tf-script/main.tf
+++ Udemy/TerraformGCP/Section3/first-tf-script/main.tf
@@ -1,13 +1,21 @@
 # Create a resource block
-# resource type = local_file (provided by resource provider)
+# resource type = local_sensitive_file (provided by resource provider)
 # resource name = sample_res (we provide this)
-resource local_file sample_res {
+#
+# Note: local_sensitive_file will not display the content during "terraform plan"
+# + resource "local_sensitive_file" "sample_res" {
+#      + content              = (sensitive value)
+# local_sensitive_file also sets file permission to 0700 instead of 0777 by default
+resource local_sensitive_file sample_res {
     # These are resource arguments/attributes
     
     # file name
     filename = "sample.txt"
 
-    # file content
+    # file content (sensitive, won't display in logging)
     content = "I love Terraform"
+
+    # Sets file permission to root only (this is default for local_sensitive_file)
+    file_permission = "0700"
 }
 
