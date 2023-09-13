222df984c1ac0bfcda80545b2ac1bc52f25ed0f3
Lee Ballard <lee.ballard@yahoo.com>
2023-09-13 13:44:01 -0400

    Section 3.1 First terraform file that generates a text file using a standard local provider.

---
+++ Udemy/TerraformGCP/Section3/first-tf-script/main.tf
@@ -0,0 +1,13 @@
+# Create a resource block
+# resource type = local_file (provided by resource provider)
+# resource name = sample_res (we provide this)
+resource local_file sample_res {
+    # These are resource arguments/attributes
+    
+    # file name
+    filename = "sample.txt"
+
+    # file content
+    content = "I love Terraform"
+}
+
